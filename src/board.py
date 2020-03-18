4dcf36922e2ecfea6f45ecfec939824a06920029
Alex Coupe <acoupe@gmail.com>
2020-03-18 16:52:52 +0000

    Board turned into a grid

--- src/board.py
+++ src/board.py
@@ -1,18 +1,26 @@
 
 class Board:
-    ROWS = 8
-    COLS = 8
+    ROWS = ['A','B','C','D','E','F','G','H']
+    COLS = ['1','2','3','4','5','6','7','8']
+    board = list()
 
     def init_board(self):
-       board = []
-       for x in range(self.COLS):
-           for y in range(self.ROWS):
-               board.append("*")
+       for x in self.COLS:
+           position = 0
+           for _y in range(8):
+               self.board.append(self.ROWS[position] + x)
+               position+=1
+               
 
     def print_board(self):
-        for x in range(self.COLS):
-            print("")
-            for y in range(self.ROWS):
-                print("* ", end = '')
-
+        characters = 0
+        print(self.board[8])
+        for x in self.board:
+            if characters < 7:
+                print(x + ' ',end='')
+                characters+=1
+            else:
+                print(x)
+                characters = 0
+           
         
